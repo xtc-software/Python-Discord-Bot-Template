0281504067ba45e7f470365e8b152411631704ff
xtc-software <dillon6654@gmail.com>
2023-08-13 23:44:57 -0700

    Init

--- bot.py
+++ bot.py
@@ -4,12 +4,7 @@ from discord import app_commands, ui
 from discord.app_commands import Choice
 from discord.ext import commands
 from discord.ext import tasks
-import datetime
 import os
-import aiosqlite #can change based off of db used for project
-import nacl #to join voice channels
-import random
-import requests
 
 #configuration
 class ViewPersistence(commands.Bot):
@@ -20,7 +15,8 @@ class ViewPersistence(commands.Bot):
         super().__init__(description=description, command_prefix=prefix, intents=intents)
         
     async def setup_hook(self) -> None: #for persistent views, add the view here
-        self.add_view()
+        # self.add_view()
+        return
 client = ViewPersistence()
 
 
@@ -29,7 +25,7 @@ global guild
 global embedColor
 global guildid
 
-guild = discord.Object(id="guildid")
+guild = discord.Object(id="1038598934265864222")
 embedColor = discord.Color.from_str("#ffffff")
 
 #functions
@@ -68,23 +64,25 @@ async def loadCogsAndExtensions():
 
 @client.tree.command(name="sync", description="Sync command tree", guild=guild) #useful for catching the bot up to servers that haven't received updates.
 async def sync(interaction: discord.Interaction):
-    if interaction.user.id == interaction.guild.owner:
+    if interaction.user == interaction.guild.owner:
         await client.tree.sync()
         await interaction.response.send_message("Synced client command tree.", ephemeral=True)
     else:
-        embed = await embeds.Error.build("You do not have permission to use this command.")
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.response.send_message("Error: You do not have permissions to run this command.", ephemeral=True)
 
 @client.tree.command(name="reload", description="Reloads a specific cog", guild=guild) #useful for hot-reloading commands that have been changed or edited since the bot has come online.
 @app_commands.choices(type=[app_commands.Choice(name="Basic", value="basic"), app_commands.Choice(name="Premium", value="premium")])
 async def reload(interaction: discord.Interaction, type: str, cog: str):
-    try:
-        await client.reload_extension(f"cogs.{type}.{cog}") #cogs.basic.{cog} basically becomes ./cogs/basic/cogname.py
-        await interaction.response.send_message(f"Reloaded {cog}", ephemeral=True)
-    except Exception as e:
-        print(e)
-        await client.load_extension(f"cogs.{type}.{cog}")
-        await interaction.response.send_message(f"Loaded {cog}", ephemeral=True)
+    if interaction.user == interaction.guild.owner:
+        try:
+            await client.reload_extension(f"cogs.{type}.{cog}") #cogs.basic.{cog} basically becomes ./cogs/basic/cogname.py
+            await interaction.response.send_message(f"Reloaded {cog}", ephemeral=True)
+        except Exception as e:
+            print(e)
+            await client.load_extension(f"cogs.{type}.{cog}")
+            await interaction.response.send_message(f"Loaded {cog}", ephemeral=True)
+    else:
+        await interaction.response.send_message("Error: You do not have permissions to run this command.", ephemeral=True)
             
 
 @client.event
@@ -92,7 +90,7 @@ async def on_ready():
     #guild = client.get_guild(guildid)
     #members = len([x for x in guild.members if not x.bot])
     prep = await loadCogsAndExtensions() #load out our bot files so we may count success and fails
-    guild = client.get_guild(guildid) #find host guild for any administrative bot tasks (such as reloading cogs)
+    guild = client.get_guild(1038598934265864222) #find host guild for any administrative bot tasks (such as reloading cogs)
     await client.tree.sync(guild=guild) #sync command tree to host guild so host guild is always up to date with commands.
     print(f"Login successful.\nBot User: {client.user}\nSuccessful Basic Cogs: {len(prep[0])}\nFailed Basic Cogs: {len(prep[1])}\nSuccessful Premium Cogs: {len(prep[2])}\nFailed Premium Cogs: {len(prep[3])}\nSuccessful Extensions: {len(prep[4])}\nFailed Extensions: {len(prep[5])}")
 client.run(os.getenv("token"))
