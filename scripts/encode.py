f84ba3ac0d26fef265847042b92fda3df4b1b850
anuraag <anuraag.warudkar@gmail.com>
2023-08-20 03:11:58 -0400

    add not working encryption file

--- scripts/encode.py
+++ scripts/encode.py
@@ -1,4 +1,9 @@
 import base64
+import os
+from cryptography.hazmat.primitives import serialization, hashes, padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 async def encode(string: str):
     string_bytes = string.encode("ascii")
@@ -12,8 +17,97 @@ async def decode(string: str):
     decoded_string = decoded_bytes.decode("ascii")
     return decoded_string
 
-async def encrypt(string: str):
-    pass
+class AdvancedEncryptor:
+    def __init__(self, kek_password):
+        self.kek_password = kek_password.encode('utf-8')
+        self.kek = None  # Key Encryption Key
+        self.generate_kek()
 
-async def decrypt(string: str):
-    pass
+    def generate_kek(self):
+        # Generate a salt for PBKDF2
+        salt = os.urandom(16)
+        
+        # Create a Key Derivation Function (KDF) instance with SHA256
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            iterations=100000,  # Number of iterations
+            salt=salt,  # Salt for added security
+            length=32  # Desired key length
+        )
+        
+        # Derive the KEK from the provided password using PBKDF2
+        kek_key = kdf.derive(self.kek_password)
+        self.kek = kek_key
+
+    def encrypt(self, plaintext):
+        # Generate a new Data Encryption Key (DEK) for each encryption
+        dek = os.urandom(32)  # Random DEK
+        iv = os.urandom(16)   # Initialization vector for AES in CFB mode
+        
+        # Create an AES cipher in CFB mode with the generated DEK and IV
+        cipher = Cipher(algorithms.AES(dek), modes.CFB(iv))
+        encryptor = cipher.encryptor()
+        
+        # Encrypt the plaintext using the AES cipher
+        ct = encryptor.update(plaintext.encode('utf-8')) + encryptor.finalize()
+
+        # Encrypt the DEK with the KEK using RSA
+        rsa_private_key = serialization.load_pem_private_key(self.kek)
+        encrypted_dek = rsa_private_key.public_key().encrypt(
+            dek,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+        # Combine the encrypted DEK, IV, and ciphertext for storage or transmission
+        return encrypted_dek + iv + ct
+
+    def decrypt(self, ciphertext):
+        # Separate the encrypted DEK, IV, and ciphertext from the input
+        encrypted_dek = ciphertext[:256]  # Assuming RSA encrypted DEK is 256 bytes
+        iv = ciphertext[256:272]
+        ct = ciphertext[272:]
+
+        # Decrypt the DEK with the KEK using RSA
+        rsa_private_key = serialization.load_pem_private_key(self.kek)
+        dek = rsa_private_key.decrypt(
+            encrypted_dek,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+        # Create an AES cipher in CFB mode with the decrypted DEK and IV
+        cipher = Cipher(algorithms.AES(dek), modes.CFB(iv))
+        decryptor = cipher.decryptor()
+        
+        # Decrypt the ciphertext using the AES cipher
+        pt = decryptor.update(ct) + decryptor.finalize()
+
+        return pt.decode('utf-8')
+
+
+def main():
+    kek_password = "YourSuperSecretKEKPassword"
+    encryptor = AdvancedEncryptor(kek_password)
+
+    original_text = "Hello, this is a secret message!"
+    
+    # Encrypt the original text
+    encrypted_text = encryptor.encrypt(original_text)
+    
+    # Decrypt the encrypted text
+    decrypted_text = encryptor.decrypt(encrypted_text)
+
+    print("Original Text:", original_text)
+    print("Encrypted Text:", encrypted_text)
+    print("Decrypted Text:", decrypted_text)
+
+
+if __name__ == "__main__":
+    main()
